ab50313d24df32458c9d9df19fb2383a0fb8b989
CCShoop <caelantennis@gmail.com>
2024-05-14 10:27:55 -0400

    v2.0.2 Logger fix

--- bot.py
+++ bot.py
@@ -24,11 +24,11 @@ logger.setLevel(logging.DEBUG)
 formatter = logging.Formatter('[%(asctime)s] [%(levelname)s] [%(message)s]')
 
 file_handler = logging.FileHandler('scheduler.log')
-file_handler.setLevel(logging.warn)
+file_handler.setLevel(logging.DEBUG)
 file_handler.setFormatter(formatter)
 
 console_handler = logging.StreamHandler()
-console_handler.setLevel(logging.debug)
+console_handler.setLevel(logging.DEBUG)
 console_handler.setFormatter(formatter)
 
 logger.addHandler(file_handler)
