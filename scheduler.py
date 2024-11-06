fe88194c1ab72cf3b4f8aab94eecd114bf0e2097
CCShoop <caelanshoop@gmail.com>
2024-11-05 22:47:47 -0500

    v2.7.8 apparently, discord cannot count

--- scheduler.py
+++ scheduler.py
@@ -1006,8 +1006,8 @@ class AvailabilityButtons(View):
         async def respond_button_callback(interaction: Interaction):
             try:
                 am_title = f'Availability for {self.event}'
-                if len(am_title) > 45:
-                    am_title = f"{am_title[:41]}..."
+                if len(am_title) >= 45:
+                    am_title = f"{am_title[:40]}..."
                 await interaction.response.send_modal(AvailabilityModal(event=self.event, title=am_title))
             except Exception as e:
                 logger.exception(f'Error sending availability modal: {e}')
@@ -1119,8 +1119,8 @@ class AvailabilityButtons(View):
             self.event.changed = True
             self.event.ready_to_create = False
             title = f"Cancel {self.event.name}"
-            if len(title) > 45:
-                title = f"{title[:41]}..."
+            if len(title) >= 45:
+                title = f"{title[:40]}..."
             await interaction.response.send_modal(CancelModal(event=self.event, title=title))
             persist.write(client.get_events_dict())
         button.callback = cancel_button_callback
