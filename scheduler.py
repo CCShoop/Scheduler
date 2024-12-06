6ec44f3237d70c5a9ff297d3ca3fa6cccf7b6705
CCShoop <caelanshoop@gmail.com>
2024-12-06 18:49:01 -0500

    v2.9.56 mention location instead of just name

--- scheduler.py
+++ scheduler.py
@@ -694,7 +694,7 @@ class Event:
                         value=self.duration_string,
                         inline=False)
         embed.add_field(name="Location",
-                        value=self.voice_channel.name,
+                        value=self.voice_channel.mention,
                         inline=False)
         embed.add_field(name="Multi Event",
                         value=f"{self.multi_event}",
