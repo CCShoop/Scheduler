bf792845007031311881ea8d0a48a11caed49f01
CCShoop <caelanshoop@gmail.com>
2025-12-29 22:21:48 -0500

    v2.15.2 availability error

--- scheduler.py
+++ scheduler.py
@@ -2224,6 +2224,9 @@ class AvailabilityModal(Modal):
                 for other_event in client.events:
                     await other_event.update_messages()
         except Exception as e:
+            embed = Embed(title="Error",
+                          color=Color.red,
+                          description=e.__str__())
             logger.exception(f"[{self.event}] Error setting specific availability: {e}")
         embed = get_participants_other_unanswered_events_embed(self.event, self.participant)
         if embed:
