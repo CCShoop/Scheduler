beef941e2aa34a084d31df50da2a92ba0744e41e
CCShoop <caelanshoop@gmail.com>
2026-01-19 22:25:46 -0500

    v2.16.0 hopefully fixes bug of events not being added to event list

--- scheduler.py
+++ scheduler.py
@@ -3515,8 +3515,8 @@ async def create(event_name: str,
                   image_url=image_url,
                   duration=duration,
                   start_times=start_times)
-    await event.make_scheduled_events()
     client.events.append(event)
+    await event.make_scheduled_events()
 
     remove_times_from_availabilities_for_events()
     await event.update_event_buttons_message()
